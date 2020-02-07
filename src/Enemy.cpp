a40558936771e64d3393f561f5390a5e45f58c68
Josh <jbromber@my.centennialcollege.ca>
2020-02-07 11:46:57 -0500

    Added Sprite Rendering to Enemies
    Danger! Missing Sprites!

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -5,15 +5,21 @@ Enemy::Enemy(){/*DANGER! Do not use!*/ }
 
 Enemy::Enemy(Frame enemyFrame, int enemyHealth, AI* enemyAI, glm::vec2 targetTransform)
 {
+	name = typeid(enemyAI).name();
+	name = name.substr(0, name.length - 2);
 	frame = enemyFrame;
 	frame.Initialize(this);
 	health = enemyHealth;
 	aI = enemyAI;
 	setPosition(targetTransform);
+	TheTextureManager::Instance()->load("../Assets/textures/"+name+".png",
+		name, TheGame::Instance()->getRenderer());
 }
 
 Enemy::~Enemy()
 {
+	TheTextureManager::Instance()->draw("enemy", getPosition().x, getPosition().y, frame.getGridSize()*frame.GridWidth(), frame.getGridSize() * frame.GridHeight(),
+		TheGame::Instance()->getRenderer());
 }
 
 void Enemy::Damage(int i)
