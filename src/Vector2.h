1c819afde4c429961d8e5ea7197b64c17601afc4
XeonDigital <xinrongisaweosm2@gmail.com>
2020-03-07 23:41:14 -0500

    added Vector2 class

---
+++ src/Vector2.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef __VECTOR_2__
+#define __VECTOR_2__
+
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(float xAxis, float yAxis);
+	~Vector2();
+	
+	float x;
+	float y;
+private:
+};
+
+#endif 
