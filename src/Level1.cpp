cb83b3a9255c2546fdf6465fae0e4c65c881716c
Jinkyu Choi <jchoi123@my.centennialcollege.ca>
2020-03-08 21:25:28 -0400

    guardian position fix

--- src/Level1.cpp
+++ src/Level1.cpp
@@ -6,12 +6,14 @@ Level1::Level1()
 {
 	loadAllTextures();
 	int second = 60;
-	int wave = 3 * second;
-	glm::vec2 bottom = glm::vec2(Config::SCREEN_WIDTH + 25, Config::SCREEN_HEIGHT + 25);
+	int wave = 5 * second;
+	glm::vec2 bottom = glm::vec2(Config::SCREEN_WIDTH + 25, Config::SCREEN_HEIGHT - 25);
 	glm::vec2 middle = glm::vec2(Config::SCREEN_WIDTH + 25, Config::SCREEN_HEIGHT / 2 + 25);
 	glm::vec2 top = glm::vec2(Config::SCREEN_WIDTH + 25, 25);
 	glm::vec2 centerTop = glm::vec2(Config::SCREEN_WIDTH + 25, Config::SCREEN_HEIGHT / 4 + 25);
-	glm::vec2 centerBottom = glm::vec2(Config::SCREEN_WIDTH + 25, 3 * Config::SCREEN_HEIGHT / 4 + 25);
+	glm::vec2 centerBottom = glm::vec2(Config::SCREEN_WIDTH + 25, 3 * Config::SCREEN_HEIGHT / 4 - 25);
+
+	glm::vec2 guardianPosition = glm::vec2(Config::SCREEN_WIDTH - 20, Config::SCREEN_HEIGHT / 2);
 
 	//main enemy
 	cannoneerSpawnTimer =
@@ -21,26 +23,26 @@ Level1::Level1()
 		wave,
 		wave + 1,
 		wave + 2,
-		//7
-		7 * wave,
-		7 * wave + 1,
-		7 * wave + 2,
-		7 * wave + 3,
-		//8
-		8 * wave,
-		8 * wave + 1,
-		8 * wave + 2,
-		8 * wave + 3,
-		//9
-		9 * wave,
-		9 * wave + 1,
-		9 * wave + 2,
-		9 * wave + 3,
 		//10
 		10 * wave,
 		10 * wave + 1,
 		10 * wave + 2,
 		10 * wave + 3,
+		//11
+		11 * wave,
+		11 * wave + 1,
+		11 * wave + 2,
+		11 * wave + 3,
+		//12
+		12 * wave,
+		12 * wave + 1,
+		12 * wave + 2,
+		12 * wave + 3,
+		//13
+		13 * wave,
+		13 * wave + 1,
+		13 * wave + 2,
+		13 * wave + 3
 	};
 	cannoneerSpawnLocation =
 	{
@@ -48,26 +50,26 @@ Level1::Level1()
 		top,
 		middle,
 		bottom,
-		//7
-		top,
-		centerTop,
-		middle,
-		centerBottom,
-		//8
-		centerTop,
-		middle,
-		centerBottom,
-		bottom,
-		//9
-		top,
-		middle,
-		centerBottom,
-		bottom,
 		//10
 		top,
 		centerTop,
 		middle,
+		centerBottom,
+		//11
+		centerTop,
+		middle,
+		centerBottom,
 		bottom,
+		//12
+		top,
+		middle,
+		centerBottom,
+		bottom,
+		//13
+		top,
+		centerTop,
+		middle,
+		bottom
 	};
 
 	zigzagSpawnTimer =
@@ -75,14 +77,14 @@ Level1::Level1()
 		//2
 		2 * wave,
 		2 * wave + 1,
-		2 * wave + 2,
+		2 * wave + 2
 	};
 	zigzagSpawnLocation =
 	{
 		//2
 		centerTop,
 		middle,
-		centerBottom,
+		centerBottom
 	};
 
 
@@ -90,16 +92,16 @@ Level1::Level1()
 	{
 		//5
 		5 * wave,
-		//11
-		11 * wave,
+		//14
+		14 * wave
 	};
 
 	guardianSpawnLocation =
 	{
 		//5
-		middle,
-		//11
-		middle
+		guardianPosition,
+		//14
+		guardianPosition
 	};
 
 
@@ -110,10 +112,10 @@ Level1::Level1()
 		3 * wave + 30,
 		3 * wave + 31,
 		3 * wave + 32,
-		//10
-		10 * wave + 30,
-		10 * wave + 31,
-		10 * wave + 32,
+		//13
+		13 * wave + 30,
+		13 * wave + 31,
+		13 * wave + 32
 	};
 
 	ramSpawnLocation =
@@ -122,10 +124,10 @@ Level1::Level1()
 		top,
 		middle,
 		bottom,
-		//10
+		//13
 		top,
 		middle,
-		bottom,
+		bottom
 	};
 
 	diagonSpawnTimer =
@@ -134,10 +136,10 @@ Level1::Level1()
 		4 * wave + 30,
 		4 * wave + 31,
 		4 * wave + 32,
-		//8
-		8 * wave + 30,
-		8 * wave + 31,
-		8 * wave + 32,
+		//11
+		11 * wave + 30,
+		11 * wave + 31,
+		11 * wave + 32
 	};
 
 	diagonSpawnLocation =
@@ -146,33 +148,33 @@ Level1::Level1()
 		top,
 		middle,
 		bottom,
-		//8
+		//11
 		top,
 		middle,
-		bottom,
+		bottom
 	};
 
-	blastSpawnTimer =
-	{
-		//5
-		5 * wave + 30,
-		5 * wave + 31,
-		//11
-		11 * wave + 30,
-		11 * wave + 31,
-		11 * wave + 32,
-	};
+	//blastSpawnTimer =
+	//{
+	//	//5
+	//	5 * wave + 30,
+	//	5 * wave + 31,
+	//	//13
+	//	13 * wave + 30,
+	//	13 * wave + 31,
+	//	13 * wave + 32
+	//};
 
-	blastSpawnLocation =
-	{
-		//5
-		centerTop,
-		centerBottom,
-		//11
-		centerTop,
-		middle,
-		centerBottom,
-	};
+	//blastSpawnLocation =
+	//{
+	//	//5
+	//	centerTop,
+	//	centerBottom,
+	//	//13
+	//	centerTop,
+	//	middle,
+	//	centerBottom
+	//};
 
 	//obstacles
 	//not yet implemented
