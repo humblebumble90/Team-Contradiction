f7daf5da8f89c80c69e89f8c22164b549c8544fd
Josh <jbromber@my.centennialcollege.ca>
2020-04-14 14:13:57 -0400

    Fixed Enemy Multiple Destruction
    Easy fix

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -63,7 +63,7 @@ void Enemy::Damage(int i)
 				return;
 			}
 			TheGame::Instance()->spawnExplosion(getPosition());
-			if (((FlyOntoScreenAI*)aI)->isBoss) {
+			if (((FlyOntoScreenAI*)aI)->isBoss && name != "Blast" && name != "Diagon" && name != "Cannoneer" && name != "Ram" && name != "Zigzag" && name != "Guardian" && !name.find("Barge")) {
 				try {
 					((Level3*)TheGame::Instance()->getScene())->CheatCode();
 				}
